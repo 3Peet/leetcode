eea9ef3b5633e5df7f64067ff45ee4de49eec41f
3Peet <mypeet096@gmail.com>
2021-11-14 17:49:17 +0700

    bit manipulation

---
+++ 231-powerOfTwo.js
@@ -0,0 +1,10 @@
+var isPowerOfTwo = function(n) {
+   let binary = n.toString(2);
+   if(binary[0] != 1) return false
+    for(let i =1;i<=binary.length;i++){
+       
+        if(binary[i] != 0) return false
+    }
+    return true;
+};
+console.log(isPowerOfTwo(65537))
