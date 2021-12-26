1774e587d62f2e74d0f41b9cc9a0c5864abc46a6
3Peet <mypeet096@gmail.com>
2021-12-26 16:55:39 +0700

    string problem

---
+++ 1689-PartitioningIntoMinimumNumberOfDeci-BinaryNumbers.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} n
+ * @return {number}
+ */
+var minPartitions = function (n) {
+	console.log(...n);
+	return Math.max(...n);
+};
+console.log(minPartitions("27346209830709182346"));
+
+// 01= 1
+// 10 =2
+// 11 =3
